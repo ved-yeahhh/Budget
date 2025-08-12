2b4fa8d258d5906f7a13faeeac36c064208e38f4
Vedant jagtap <jvedant2109@gmail.com>
2025-08-12 20:22:19 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/codingliquids/budget_app_complete_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codingliquids.budgetAppComplete_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+import 'firebase_options.dart';
+import 'responsive_handler.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    setPathUrlStrategy();
+  }
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await GoogleSignIn.instance.initialize();
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Budget App",
+      //  theme: ThemeData(useMaterial3: true),
+      home: ResponsiveHandler(),
+    );
+  }
+}
